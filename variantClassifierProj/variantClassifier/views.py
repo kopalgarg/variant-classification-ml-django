4bfa806b80a7124f302f153371df71eaede0b953
kopalgarg <kopalgarg24@gmail.com>
2021-05-12 18:34:07 -0600

    create a home page and results view

--- variantClassifierProj/variantClassifier/views.py
+++ variantClassifierProj/variantClassifier/views.py
@@ -1,8 +1,12 @@
 from django.http import HttpResponse
 from django.shortcuts import render
-
+import joblib
 def home(request):
     return render(request, "home.html")
 
 def result(request):
+    # load model
+    dtree = joblib.load('final_model.sav')
+    input = request.GET('file')
+    input.head()
     return render(request, "result.html")
